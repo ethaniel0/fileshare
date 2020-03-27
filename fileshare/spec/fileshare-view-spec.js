2c7c4045c465cd5f171d8938e170ba10bed28481
Ethaniel <37252200+ethaniel0@users.noreply.github.com>
2020-03-27 13:21:19 -0400

    Add files via upload

---
+++ fileshare/spec/fileshare-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FileshareView from '../lib/fileshare-view';
+
+describe('FileshareView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
